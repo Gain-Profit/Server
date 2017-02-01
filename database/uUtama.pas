6288f566ee57cebf3aa4fbcc276c52867cc0b330
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-02-01 15:44:51 +0700

    initial database app

---
+++ database/uUtama.pas
@@ -0,0 +1,24 @@
+unit uUtama;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TFUtama = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FUtama: TFUtama;
+
+implementation
+
+{$R *.dfm}
+
+end.
