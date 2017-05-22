c7e8e78212dd87f26059faf699316c5ab017c599
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-22 17:18:46 +0700

    cek expired.

--- UpdateApplication/source/main/ViewModel.Main.pas
+++ UpdateApplication/source/main/ViewModel.Main.pas
@@ -3,7 +3,7 @@ unit ViewModel.Main;
 interface
 
 uses
-  Model.Main, ApiUtils;
+  Model.Main, ApiUtils, System.DateUtils, System.SysUtils;
 
 type
   TProci = reference to procedure (msg: string);
@@ -11,15 +11,18 @@ type
   TViewModelMain = class
   private
     FApi: TFirebaseApi;
-    FClient : TClient;
-    FOnShowMessage : TProci;
+    FClient: TClient;
+    FOnShowMessage: TProci;
+    FOnLoadClient: TProc<TClient>;
     FStarted: Boolean;
     function GetClient: TClient;
+    function GetNow: LongInt;
   public
     constructor Create;
     destructor Destroy;
     procedure Start(ACid: string);
     procedure SetOnShowMessage(AProc: TProci);
+    procedure SetOnLoadClient(AProc: TProc<TClient>);
   published
     property Client: TClient read GetClient;
   end;
@@ -51,6 +54,20 @@ begin
   Result := FClient;
 end;
 
+function TViewModelMain.GetNow: LongInt;
+var
+  LStart: TDate;
+begin
+  // let's start from 2015
+  LStart := EncodeDate(2015,1,1);
+  Result := DaysBetween(LStart, Date);
+end;
+
+procedure TViewModelMain.SetOnLoadClient(AProc: TProc<TClient>);
+begin
+  FOnLoadClient := AProc;
+end;
+
 procedure TViewModelMain.SetOnShowMessage(AProc: TProci);
 begin
   FOnShowMessage := AProc;
@@ -59,6 +76,14 @@ end;
 procedure TViewModelMain.Start(ACid: string);
 begin
   FClient := FApi.GetAsT<TClient>(PATH_CLIENT + ACid);
+  if (Assigned(FOnLoadClient)) then
+    FOnLoadClient(FClient);
+
+  if FClient.Expired < GetNow then
+  begin
+    FOnShowMessage('Masa Garansi Sudah Expired');
+    Exit;
+  end;
 end;
 
 end.
