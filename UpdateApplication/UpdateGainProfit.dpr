273b4e7d4b9b232b58a9df32b699eea56e4aee8f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-22 13:46:35 +0700

    add viewmodel dan apiutils

--- UpdateApplication/UpdateGainProfit.dpr
+++ UpdateApplication/UpdateGainProfit.dpr
@@ -3,7 +3,9 @@ program UpdateGainProfit;
 uses
   Vcl.Forms,
   View.Main in 'source\main\View.Main.pas' {FrmMain},
-  Model.Main in 'source\main\Model.Main.pas';
+  Model.Main in 'source\main\Model.Main.pas',
+  ApiUtils in 'source\ApiUtils.pas',
+  ViewModel.Main in 'source\main\ViewModel.Main.pas';
 
 {$R *.res}
 
