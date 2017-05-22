dccd0652037a20fca61cf3c2341c9d2739f94abf
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-22 18:31:30 +0700

    execute sql and getClientId

--- UpdateApplication/source/DbUtils.pas
+++ UpdateApplication/source/DbUtils.pas
@@ -9,13 +9,19 @@ type
   TDatabase = class(TObject)
   private
     FConnection: TMyConnection;
+    FQuery: TMyQuery;
   public
     constructor Create;
     destructor Destroy;
+    procedure ExecSQL(ASQL: string);
     function IsConnected: Boolean;
+    function GetClientId: string;
   end;
 
 implementation
+
+const
+  CLIENT_SQL = 'SELECT kd_perusahaan FROM tb_company LIMIT 1';
 
 function Decrypt(const s: string; CryptInt: Integer): string;
 var
@@ -36,6 +42,8 @@ var
   X: TextFile;
 begin
   FConnection := TMyConnection.Create(nil);
+  FQuery := TMyQuery.Create(FConnection);
+  FQuery.Connection := FConnection;
 
   assignfile(X, 'koneksi.cbCon');
   reset(X);
@@ -61,6 +69,23 @@ end;
 destructor TDatabase.Destroy;
 begin
   FConnection.Free;
+end;
+
+procedure TDatabase.ExecSQL(ASQL: string);
+begin
+  with FQuery do
+  begin
+    Close;
+    SQL.Clear;
+    SQL.Text := ASQL;
+    Open;
+  end;
+end;
+
+function TDatabase.GetClientId: string;
+begin
+  ExecSQL(CLIENT_SQL);
+  Result := FQuery.FieldByName('kd_perusahaan').AsString;
 end;
 
 function TDatabase.IsConnected: Boolean;
