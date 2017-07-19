fa7db82e283d61f270833195bd1c84b50b9e4143
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-19 21:17:57 +0700

    Tambah Form Setting ke project.

--- UpdateApplication/UpdateGainProfit.dpr
+++ UpdateApplication/UpdateGainProfit.dpr
@@ -7,7 +7,9 @@ uses
   ApiUtils in 'source\ApiUtils.pas',
   ViewModel.Main in 'source\main\ViewModel.Main.pas',
   DbUtils in 'source\DbUtils.pas',
-  FileUtils in 'source\FileUtils.pas';
+  FileUtils in 'source\FileUtils.pas',
+  View.Setting in 'source\setting\View.Setting.pas' {FrmSetting},
+  ViewModel.Setting in 'source\setting\ViewModel.Setting.pas';
 
 {$R *.res}
 
@@ -15,5 +17,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmMain, FrmMain);
+  Application.CreateForm(TFrmSetting, FrmSetting);
   Application.Run;
 end.
