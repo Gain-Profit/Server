67870d3c66115c14f009df929db428772e26763f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-19 21:17:03 +0700

    Form Setting

---
+++ UpdateApplication/source/setting/View.Setting.pas
@@ -0,0 +1,24 @@
+unit View.Setting;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmSetting = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmSetting: TFrmSetting;
+
+implementation
+
+{$R *.dfm}
+
+end.
