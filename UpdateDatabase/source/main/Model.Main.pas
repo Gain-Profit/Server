5408cd6fccfcec8c9eb7e279edccf18ccaa6105c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-24 23:46:28 +0700

    model TDbVersion
    property on TClient read only.

--- UpdateDatabase/source/main/Model.Main.pas
+++ UpdateDatabase/source/main/Model.Main.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.DateUtils;
 
 type
-    TClient = class(TObject)
+  TClient = class(TObject)
   private
     FKode: string;
     FNama: string;
@@ -18,10 +18,22 @@ type
       AExpired: LongInt); overload;
     function GetExpiredDate: TDate;
   published
-    property Kode: string read FKode write FKode;
-    property Nama: string read FNama write FNama;
-    property Alamat: string read FAlamat write FAlamat;
-    property Expired: LongInt read FExpired write FExpired;
+    property Kode: string read FKode;
+    property Nama: string read FNama;
+    property Alamat: string read FAlamat;
+    property Expired: LongInt read FExpired;
+  end;
+
+  TDbVersion = class
+    FId: string;
+    FVersion: string;
+    FUrl: string;
+  public
+    constructor Create(AId, AVersion, AUrl: string);
+  published
+    property Id: string read FID;
+    property Version: string read FVersion;
+    property Url: string read FUrl;
   end;
 
 implementation
@@ -47,6 +59,15 @@ var
 begin
   LStart := EncodeDate(2015, 1, 1);
   Result := IncDay(LStart, FExpired);
+end;
+
+{ TDbVersion }
+
+constructor TDbVersion.Create(AId, AVersion, AUrl: string);
+begin
+  FId := AId;
+  FVersion := AVersion;
+  FUrl := AUrl;
 end;
 
 end.
